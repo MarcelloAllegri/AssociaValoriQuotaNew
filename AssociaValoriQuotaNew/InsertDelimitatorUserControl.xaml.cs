63fbc8e0280fbb3922fa52429fac7917d62bd8d5
Marcello Allegri <marcello.allegri@hotmail.it>
2019-02-23 19:13:54 +0100

    primi controlli

---
+++ AssociaValoriQuotaNew/InsertDelimitatorUserControl.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AssociaValoriQuotaNew
+{
+    /// <summary>
+    /// Logica di interazione per InsertDelimitatorUserControl.xaml
+    /// </summary>
+    public partial class InsertDelimitatorUserControl : UserControl
+    {
+        private bool m_IsInputDelimiter;
+
+        public bool IsInputDelimiter
+        {
+            set { m_IsInputDelimiter = value; }
+            get { return m_IsInputDelimiter; }
+        }
+
+        public InsertDelimitatorUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public string Save()
+        {
+            return this.CharacterDelimiterTextBox.Text;
+        }
+
+    }
+}
--- AssociaValoriQuotaNew/MainWindow.xaml.cs
+++ AssociaValoriQuotaNew/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System;
+﻿using AssociaValoriQuotaNew.Resources.Classes;
+using Microsoft.Win32;
+using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AssociaValoriQuotaNew
 {
@@ -20,6 +15,30 @@ namespace AssociaValoriQuotaNew
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<FileInformation> m_fileInformation;
+        Dictionary<string, char> ListOfSeparator;
+        private ConcurrentBag<string> m_FileContentList;
+        private ConcurrentBag<string> m_OutputFileContentList;
+        private char m_ImputFileDelimiter;
+        private char m_OutputFileDelimiter;
+        private int m_ItemPorRow = -1;
+
+        public List<FileInformation> fileInformation
+        {
+            get { return m_fileInformation; }
+            set { m_fileInformation = value; }
+        }
+
+        public char InputFileDelimiter
+        {
+            set { m_ImputFileDelimiter = value; }
+        }
+
+        public char OutputFileDelimiter
+        {
+            set { m_ImputFileDelimiter = value; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,23 +46,130 @@ namespace AssociaValoriQuotaNew
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            ListOfSeparator = new Dictionary<string, char>();
 
+            ListOfSeparator.Add(".", '.');
+            ListOfSeparator.Add(",", ',');
+            ListOfSeparator.Add(";", ';');
+            ListOfSeparator.Add("Tab", Convert.ToChar(11));
+            ListOfSeparator.Add("Space", ' ');
+            ListOfSeparator.Add("Other", 'o');
+
+            OutputFileSeparatorChooseComboBox.ItemsSource = InputFileSeparatorChooseComboBox.ItemsSource = ListOfSeparator;
         }
 
         private void AddFileToolbarButton_Click(object sender, RoutedEventArgs e)
         {
+            SelectFile();
+        }
 
+        private void SelectFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Csv file (*.csv)|*.csv|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                fileInformation = new List<FileInformation>();
+                fileInformation.Add(new FileInformation(Path.GetFileName(openFileDialog.FileName), Path.GetFullPath(openFileDialog.FileName)));
+                this.FileListView.ItemsSource = fileInformation;
+            }
+        }
+
+        private void SaveResult()
+        {
+            bool retry = true;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Title = "Salva File Risultato";
+            saveFileDialog1.Filter = "csv files (*.csv)|*.csv";
+
+            while (retry)
+            {
+                if (saveFileDialog1.ShowDialog() == true)
+                {
+                    if (File.Exists(saveFileDialog1.FileName) == false)
+                    {
+
+                        File.AppendAllLines(saveFileDialog1.FileName, m_OutputFileContentList.ToList());
+                        retry = false;
+
+                        MessageBox.Show("Procedura di sotituzione completata.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Il file e' correntemente in uso, si prega di chiuderlo.", "Errore", MessageBoxButton.OK, MessageBoxImage.Error);
+                        retry = true;
+                    }
+                }
+                else
+                {
+                    retry = true;
+                }
+            }
+        }
+
+        private int GetFile()
+        {
+            try
+            {
+                m_FileContentList = new ConcurrentBag<string>(File.ReadAllLines(m_fileInformation[0].Path));
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error during importing file. Check it!");
+                return -1;
+            }
+
+            return 0;
         }
 
         private void InputFileSeparatorChooseComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            var selectedValue = InputFileSeparatorChooseComboBox.SelectedValue;
 
+            if (selectedValue != null)
+                switch (((KeyValuePair<string, char>)selectedValue).Key)
+                {
+                    case ".":
+                    case ",":
+                    case ";":
+                    case "Tab":
+                    case "Space": this.m_ImputFileDelimiter = ListOfSeparator[((KeyValuePair<string, char>)selectedValue).Key]; break;
+                    case "Other": InsertCustomDelimiter(true); break;
+                    default:
+                        break;
+                }
         }
 
         private void OutputFileSeparatorChooseComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            var selectedValue = OutputFileSeparatorChooseComboBox.SelectedValue;
 
-        }        
+            if (selectedValue != null)
+                switch (((KeyValuePair<string, char>)selectedValue).Key)
+                {
+                    case ".":
+                    case ",":
+                    case ";":
+                    case "Tab":
+                    case "Space": this.m_OutputFileDelimiter = ListOfSeparator[((KeyValuePair<string, char>)selectedValue).Key]; break;
+                    case "Other": InsertCustomDelimiter(false); break;
+                    default:
+                        break;
+                }
+        }
+
+        private void InsertCustomDelimiter(bool isInputDelimiter)
+        {
+            NextButton.IsEnabled = false;
+            customCharacterUserControl.Visibility = Visibility.Visible;
+            customCharacterUserControl.IsSelected = true;
+            insertDelimitatorUserControl.IsInputDelimiter = isInputDelimiter;
+            MainTabItem.Visibility = Visibility.Collapsed;
+        }
 
         private void NextButton_Click(object sender, RoutedEventArgs e)
         {
@@ -76,6 +202,45 @@ namespace AssociaValoriQuotaNew
             }
 
             MainTabControl.UpdateLayout();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            string text = insertDelimitatorUserControl.Save();
+
+
+            if (text.Count() == 1)
+            {
+                switch (insertDelimitatorUserControl.IsInputDelimiter)
+                {
+                    case true: m_ImputFileDelimiter = text[0]; break;
+                    case false: m_OutputFileDelimiter = text[0]; break;
+                }
+
+                MessageBox.Show("Saved!");
+            }
+            else
+            {
+                if (text.Count() == 0)
+                    MessageBox.Show("Non è stato inserito alcun carattere!");
+                else
+                    MessageBox.Show("Hai inserito più di 1 carattere!");
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainTabItem.Visibility = Visibility.Visible;
+            MainTabItem.IsSelected = true;
+            string text = insertDelimitatorUserControl.Save();
+            if (!(text.Count() == 1))
+                switch (insertDelimitatorUserControl.IsInputDelimiter)
+                {
+                    case true: InputFileSeparatorChooseComboBox.SelectedItem = null; break;
+                    case false: OutputFileSeparatorChooseComboBox.SelectedItem = null; break;
+                }
+            customCharacterUserControl.Visibility = Visibility.Collapsed;
+            NextButton.IsEnabled = true;
         }
     }
 }
